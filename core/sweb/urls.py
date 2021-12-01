b92635305b100bc6549eb74e5538bda81fc4881b
jairodri <rodrig43@gmail.com>
2021-12-01 18:50:39 +0100

    utilizando vistas basadas en clases

--- core/sweb/urls.py
+++ core/sweb/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from core.sweb.views.formasdepago.views import formasdepago_list
+from core.sweb.views.formasdepago.views import *
 
 app_name = 'sweb'
 
 urlpatterns = [
-    path('formasdepago/list/', formasdepago_list, name='formasdepago_list'),
+    path('formasdepago/list/', FormaDePagoListView.as_view(), name='formasdepago_list'),
 ]
--- core/sweb/views/formasdepago/views.py
+++ core/sweb/views/formasdepago/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 from core.sweb.models import FormaDePago
 
 
-def formasdepago_list(request):
-    data = {
-        'title': 'Formas de Pago - Listado',
-        'formasdepago': FormaDePago.objects.all()
-    }
-    return render(request, 'formasdepago/list.html', data)
+class FormaDePagoListView(ListView):
+    model = FormaDePago
+    template_name = 'formasdepago/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Formas de Pago'
+        return context
