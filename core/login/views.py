303cbd8f1a12222202be355eb21a575f4997d23f
jairodri <rodrig43@gmail.com>
2021-12-07 20:16:22 +0100

    afinando login

---
+++ core/login/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.utils.translation import gettext, gettext_lazy as _
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    error_messages = {
+        'invalid_login': "El usuario y/o la contraseña no son correctos",
+        'inactive': "Esta cuenta está inactiva",
+    }
+
+
+class LoginFormView(LoginView):
+    template_name = 'login.html'
+    authentication_form = CustomAuthenticationForm
+
+    # sobreescribimos el método get_context_data para añadir info al contexto
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Iniciar sesión'
+        return context
+
